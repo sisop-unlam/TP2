fbb827de76221b600fef38c4a883b10a54965380
Joaquín Krasuk <jkrasuk@alumno.unlam.edu.ar>
2019-10-15 12:35:11 -0300

    Modifico teoria 1

--- ejercicio1/ejercicio1.ps1
+++ ejercicio1/ejercicio1.ps1
@@ -1,6 +1,8 @@
 ﻿Param (
+    #Path salida es el primer parametro en ser enviado, y no es obligatorio enviarlo (por default tiene procesos.txt)
     [Parameter(Position = 1, Mandatory = $false)]
     [String] $pathsalida = ".\procesos.txt ",
+    #En este caso, por default la cantidad de procesos a mostrar por pantalla sera 3
     [int] $cantidad = 3
 )
 #Verifica si existe $pathSalida
@@ -30,8 +32,9 @@ else {
     Write-Host "El path no existe"
 }
 
-# 1) El objetivo del script es escribir en un archivo (el cual por default es procesos.txt, o uno    ingresado por el usuario) todos los procesos del sistema, con su nombre y ID.
-# 2) Validaria que el path es efectivamente un archivo (si se trata de un directorio, se muestra el siguiente mensaje de error: "out-file : Access to the path 'D:\dev\sisop\TP2\ejercicio1\prueba' is denied.")
+# 1) El objetivo del script es escribir en un archivo (el cual por default es procesos.txt, o uno    ingresado por el usuario) todos los procesos del sistema, con su nombre y ID. Luego, muestra por pantalla los N primeros procesos.
+# 2) Validaria que el path es efectivamente un archivo (si se trata de un directorio, se muestra el siguiente mensaje de error: "out-file : Access to the path 'D:\dev\sisop\TP2\ejercicio1\prueba' is denied."). Otra validacion podria ser que la cantidad ingresada por parametro no sea mayor a la cantidad de profesos que efectivamente se estan corriendo al momento de la ejecucion.
+#
 # 3) Si se ejecuta el script sin ningun parametro, pueden ocurrir dos situaciones:
 #       a) Si el archivo procesos.txt no existe, se indicara que el path no existe.
 #       b) Si el archivo procesos.txt existe, entonces se comenzara a ejecutar el script y hara un append en el archivo
