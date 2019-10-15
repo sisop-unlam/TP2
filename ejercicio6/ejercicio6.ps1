e37538401d7b5119ad0a7a6c5b4681a00eef2a33
Joaquín Krasuk <jkrasuk@alumno.unlam.edu.ar>
2019-10-15 12:16:42 -0300

    Fix del ejercicio6

--- ejercicio6/ejercicio6.ps1
+++ ejercicio6/ejercicio6.ps1
@@ -19,7 +19,6 @@
     ejercicio6.ps1 -Entrada=matriz2 -Producto=10
     Toma la matriz2 y realiza el producto escalar con 10
 #>
-
 <#
 ***********************************************************************************
  Nombre Del Script:        ejercicio6.ps1
@@ -40,25 +39,36 @@
 
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory = $True)]
+    [Parameter( Mandatory = $true, ParameterSetName = "Producto")]
+    [Parameter( Mandatory = $true, ParameterSetName = "Suma")]
     [ValidateNotNullOrEmpty()]
     [string]$Entrada,
 	
-    [Parameter(Mandatory = $false)]
+    [Parameter( Mandatory = $false, ParameterSetName = "Producto")]
     [ValidateNotNullOrEmpty()]
+    [ValidateRange(2, [int]::MaxValue)]
+    [ValidatePattern("[0-9]")]
     [int]$Producto,
 
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false, ParameterSetName = "Suma")]  
     [ValidateNotNullOrEmpty()]
     [string]$Suma
 )
-
 if ($Suma -and $Producto) {
     Write-Host "Solo se admite una opcion a la vez"
     exit
 }
+if (!(Test-Path -Path $Entrada)) {
+    Write-Host "Ingrese un archivo valido."
+    exit
+}
 
 $texto_archivo = Get-Content -Path $Entrada;
+
+if ($texto_archivo.count -eq 0) {
+    Write-Host "Archivo ENTRADA vacio, verifique nuevamente"
+    exit
+}
 $renglones = ($texto_archivo.Split('\n')); #Paso las palabras a un vector.
 
 $matrizA = @()
@@ -66,7 +76,7 @@ For ($i = 0; $i -lt $renglones.Length; $i++) {
     $valores = ($renglones[$i].Split('|'));
     $arr = @()
     For ($j = 0; $j -lt $valores.Length; $j++) {
-        $arr += [int]$valores[$j] * 5;
+        $arr += [double]$valores[$j];
     }
     $matrizA += , $arr;
 }
@@ -75,14 +85,23 @@ $Entrada2 = "$Suma";
 $matrizResultante = @();
 
 if ($Entrada2 -ne "") {
+    if (!(Test-Path -Path $Entrada2)) {
+        Write-Host "Ingrese un archivo valido."
+        exit
+    }
+
     $texto_archivo2 = Get-Content -Path $Entrada2;
+    if ($texto_archivo2.count -eq 0) {
+        Write-Host "Archivo SUMA vacio, verifique nuevamente"
+        exit
+    }
     $renglones2 = ($texto_archivo2.Split('\n')); #Paso las palabras a un vector.
     $matrizB = @()
     For ($i = 0; $i -lt $renglones.Length; $i++) {
         $valores = ($renglones2[$i].Split('|'));
         $arr = @()
         For ($j = 0; $j -lt $valores.Length; $j++) {
-            $arr += [int]$valores[$j] + [int]$matrizA[$i][$j];
+            $arr += [double]$valores[$j] + [double]$matrizA[$i][$j];
         }
         $matrizB += , $arr;
     }
@@ -93,17 +112,20 @@ else {
         $valores = ($renglones[$i].Split('|'));
         $arr = @()
         For ($j = 0; $j -lt $valores.Length; $j++) {
-            $arr += [int]$valores[$j] * $Producto;
+            $arr += [double]$valores[$j] * $Producto;
         }
         $matrizResultante += , $arr;
     }
 }
 
-$salida = "./salida." + $Entrada
 
+$nombreArchivo = $Entrada.Split("\")
+$nombreArch = $nombreArchivo[$nombreArchivo.Count - 1]
 #Salida del archivo
+$salida = "$PSScriptRoot\salida.$nombreArch"
 #Creo o limoio el archivo
-Set-Content $salida -Value "" -NoNewline
+
+Set-Content "$salida" -Value "" -NoNewline
 #Imrpimo
 foreach ($element in $matrizResultante) {
     $renglon = "";
@@ -111,5 +133,5 @@ foreach ($element in $matrizResultante) {
         $renglon += [string]$i + "|";
     }
     
-    $renglon.TrimEnd("|") | Add-Content $salida
+    $renglon.TrimEnd("|") | Add-Content "$salida"
 }
